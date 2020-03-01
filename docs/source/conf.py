0d93d54fd61ff79509d2bbbe1a3a0b8359990be9
rgioai <16374493+rgioai@users.noreply.github.com>
2020-03-01 09:42:43 -0500

    Switch to read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +32,9 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme"
 ]
+
 
 # Specify master_doc is index per sphinx-quickstart and not contents
 # Per https://github.com/readthedocs/readthedocs.org/issues/2569
@@ -51,7 +54,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
